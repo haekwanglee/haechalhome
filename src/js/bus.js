d3ce01f4c21988a2fe9c0895aef2aa87b2fc34c5
hekasian <haekwangl@gmail.com>
2020-10-04 20:36:56 +0900

    1004 bus api test2

--- src/js/bus.js
+++ src/js/bus.js
@@ -1,19 +1,20 @@
 var servicekey = 'HiKEn%2Binv0Y%2BCuGvVtE6YewIhkFK2DdobGFcYnkTZklGXMupZOVb6wzxN7pncoCcRmILBwq%2F0nqnnqcuvtCuow%3D%3D';
 
 var xhr = new XMLHttpRequest();
-var url = 'http://openapi.gbis.go.kr/ws/rest/busarrivalservice/station'; /*URL*/
-var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+servicekey; /*Service Key*/
-queryParams += '&' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent('1234567890'); /**/
+var url = 'http://openapi.gbis.go.kr/ws/rest/busarrivalservice/station'; /*URL*/            
+var queryParams = '?';
+queryParams += '&' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent('HiKEn%2Binv0Y%2BCuGvVtE6YewIhkFK2DdobGFcYnkTZklGXMupZOVb6wzxN7pncoCcRmILBwq%2F0nqnnqcuvtCuow%3D%3D'); /**/
 queryParams += '&' + encodeURIComponent('stationId') + '=' + encodeURIComponent('200000078'); /**/
-xhr.open('GET', url + queryParams);
-
-var hw = document.getElementById('busbutton');
-hw.addEventListener('click', function(){
-	xhr.send('');	
-})
+queryParams += '&';
 
 xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
         alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
     }
 };
+
+var hw = document.getElementById('busbutton');
+hw.addEventListener('click', function(){
+	xhr.open('GET', url + queryParams);
+	xhr.send('');	
+})
